80caaf6031eee51a7472bcef7c51ff2005da8237
Matthew Migliore <mmigliore311@gmail.com>
2018-10-17 16:57:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, supplies|
-      puts "  #{holiday.capitalize}: #{supplies.join(', ')}"
+      puts "  #{
+        holiday.capitalize
+        
+      }: #{supplies.join(', ')}"
     end
   end
 
