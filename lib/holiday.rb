e29e1c394e4d859b99def074ccde7319d13295ee
Matthew Migliore <mmigliore311@gmail.com>
2018-10-17 17:24:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
    holidays.each do |holiday, supplies|
      supplies.each do |supply|
        if supply == "BBQ"
-         bbq_holidays.push(holidays.key(supply))
+         bbq_holidays << holidays.key(supply)
        end 
      end 
   
